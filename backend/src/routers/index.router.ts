47c13084e245d0feee0bb44f8809ae790a47bcae
5h4r0 <stephane@sharo.fr>
2025-09-17 18:02:53 +0200

    feat: First routes - / homepage, /api for backend, app listen port 3000...

---
+++ backend/src/routers/index.router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+// import { router as activitiesRouter } from "./activities.router.js";
+// import { router as categoriesRouter } from "./categories.router.js";
+// import { router as sessionsRouter } from "./sessions.router.js";
+
+export const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+// router.use("/activities", activitiesRouter);
+// router.use("/categories", categoriesRouter);
+// router.use("/sessions", sessionsRouter);
