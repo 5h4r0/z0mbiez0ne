d950e649ecd8325442d988e8ac9b747d4bb12a12
5h4r0 <stephane@sharo.fr>
2025-10-03 14:22:38 +0200

    feat: sessions controller getSessions, with zod control

--- backend/src/controllers/sessions.controller.ts
+++ backend/src/controllers/sessions.controller.ts
@@ -1,54 +1,64 @@
-import { Prisma } from "@prisma/client";
 import type { Request, Response } from "express";
 import z from "zod";
 import { getPagination } from "../helpers/index.js";
+import { BadRequestError, ConflictError, UnauthorizedError } from "../lib/errors.js";
+import { Prisma } from "@prisma/client"
 import { prisma } from "../models/index.js";
 
 
 /** get all */
-export const getSessions = (req: Request, res: Response): Promise<void> => {
-  const { take, skip } = getPagination(req);
+export const getSessions = (req: Request, res: Response) => {
+  const { take, skip } = getPagination(req)
 
-  return prisma.sessions
-    .findMany()
-    .then((sessions) => {
-      res.status(200).json({
-        success: true,
-        data: sessions,
-      }),
-      take !== undefined ? { take } : {},
-      skip !== undefined ? { skip } : {}
-      })
-    .catch((error) => {
-      console.error(`Error fetching sessions,`, error),
-      res.status(500).json({
-        success: false,
-        message: "Error fetching sessions",
-        error: error.message,
-      })
+  const paginationSchema = z.object({
+    take: z.string().optional(),
+    skip: z.string().optional()
+  })
+
+  return paginationSchema.parseAsync(req.query)
+    .then(() => {
+      const baseQuery: Parameters<typeof prisma.sessions.findMany>[0] = {
+        select: {
+          id: true,
+          activity_id: true,
+          date: true,
+          capacity: true,
+          unit_price: true,
+          status: true,
+          activity: {
+            select: { id: true, title: true, description: true }
+          },
+          orders_lines: {
+            select: { id: true, session_id: true }
+          }
+        }
+      }
+
+      return prisma.sessions.findMany(
+        Object.assign(
+          baseQuery,
+          take !== undefined ? { take } : {},
+          skip !== undefined ? { skip } : {}
+        ) as Prisma.sessionsFindManyArgs
+      )
     })
-}
-
-
-/** get one */
-export const getSession = (req: Request, res: Response): Promise<void> => {
-
-}
-
-
-/** create */
-export const createSession = (req: Request, res: Response): Promise<void> => {
-
-}
-
-
-/** update */
-export const updateSession = (req: Request, res: Response): Promise<void> => {
-
-}
-
-
-/** delete */
-export const deleteSession = (req: Request, res: Response): Promise<void> => {
-
+    .then((sessions) => {
+      return sessions.length === 0
+        ? Promise.reject(new BadRequestError("no sessions caught"))
+        : res.status(200).json({
+            success: true,
+            data: sessions
+          })
+    })
+    .catch((error) => {
+      return error instanceof z.ZodError
+        ? res.status(400).json({
+            status: "error",
+            message: error.issues.map((e) => e.message).join(", ")
+          })
+        : res.status(500).json({
+            status: "error",
+            message: error.message || "error fetching sessions"
+          })
+    })
 }
--- backend/src/routers/sessions.router.ts
+++ backend/src/routers/sessions.router.ts
@@ -5,12 +5,12 @@ export const router = Router();
 
 // public routes
 router.get("/sessions", sessionsController.getSessions);
-router.get("/sessions/:id", sessionsController.getSession);
+// router.get("/sessions/:id", sessionsController.getSession);
 
-// protected routes (admin)
-router.post("/session", sessionsController.createSession);
-router.put("/sessions/:id", sessionsController.updateSession);
-router.delete("/sessions/:id", sessionsController.deleteSession);
+// // protected routes (admin)
+// router.post("/session", sessionsController.createSession);
+// router.put("/sessions/:id", sessionsController.updateSession);
+// router.delete("/sessions/:id", sessionsController.deleteSession);
 
 
 // export default router;
