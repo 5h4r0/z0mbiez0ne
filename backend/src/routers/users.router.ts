a02800c44205040dba3b1b11a46bd73d60f1dcab
5h4r0 <stephane@sharo.fr>
2025-10-07 18:32:06 +0200

    feat: access control middleware

--- backend/src/routers/users.router.ts
+++ backend/src/routers/users.router.ts
@@ -1,15 +1,14 @@
-import { Router } from "express";
-import * as usersController from "../controllers/users.controller.js";
+import { Router, type NextFunction } from "express"
+import * as usersController from "../controllers/users.controller.js"
+import { checkRoles } from "../middlewares/access-control.middleware.js"
 
-export const router = Router();
+export const router = Router()
+// export default router
 
 // protected routes (member, admin)
-router.get("/users/:id", usersController.getUser);
-router.put("/users/:id", usersController.updateUser);
-router.delete("/users/:id", usersController.deleteUser);
+router.get("/users/:id", checkRoles(["Member", "Admin"]), usersController.getUser)
+router.put("/users/:id", checkRoles(["Member", "Admin"]), usersController.updateUser)
+router.delete("/users/:id", checkRoles(["Member", "Admin"]), usersController.deleteUser)
 
 // protected route (admin)
-router.get("/users", usersController.getUsers);
-
-
-// export default router;
+router.get("/users", checkRoles(["Admin"]), usersController.getUsers)
