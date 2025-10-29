4c22fc8187597dcb3acfe1074575ba5a94cf2281
5h4r0 <stephane@sharo.fr>
2025-10-29 20:09:26 +0100

    Biome fixes, Vitejs frontend init, WIP on routes

--- backend/src/utils/getrandomint.ts
+++ backend/src/utils/getrandomint.ts
@@ -1,7 +1,5 @@
-/** Utilitaire : entier aléatoire inclusif [min, max] */
-/**
- * Retourne un entier aléatoire inclusif [min, max]
- */
+/** Utilitaire : retourne un entier aléatoire inclusif [min, max] */
+
 export function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
