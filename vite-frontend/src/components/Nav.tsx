00c26a6690205e10c8a16c5bcfb66b05703eebae
5h4r0 <stephane@sharo.fr>
2026-05-17 18:24:44 +0200

    init frontend

--- vite-frontend/src/components/Nav.tsx
+++ vite-frontend/src/components/Nav.tsx
@@ -3,14 +3,13 @@ import { NavLink } from "react-router";
 export default function Nav() {
   return (
     <>
-      <nav className="header__nav">
-        <NavLink className="menu-link menu-link--selected" to="/">Accueil</NavLink>
-        <NavLink className="menu-link" to="/sessions">Sessions à venir</NavLink>
-        <NavLink className="menu-link" to="/activities">Activités</NavLink>
-        <NavLink className="menu-link" to="/categories">Catégories</NavLink>
-        <NavLink className="menu-link" to="/evenements">Évènements</NavLink>
-        <NavLink className="menu-link" to="/contact">Contact</NavLink>
-        <NavLink className="menu-link" to="/account">Client</NavLink>
+      <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/activities">Activités</NavLink>
+        <NavLink to="/sessions">Sessions à venir</NavLink>
+        <NavLink to="/categories">Catégories</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/account">Client</NavLink>
       </nav>
     </>
   )
